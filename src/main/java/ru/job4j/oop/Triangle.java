3b631e65d118375bf8c18d1730ec056c49d1a57a
vladimir <tintutin@list.ru>
2026-03-23 17:15:45 +0400

    Triangle

--- src/main/java/ru/job4j/oop/Triangle.java
+++ src/main/java/ru/job4j/oop/Triangle.java
@@ -12,7 +12,7 @@ public class Triangle {
     }
 
     public double semiPerimeter(double a, double b, double c) {
-        return (a + b + c)/2;
+        return (a + b + c) / 2;
     }
 
     public boolean exist(double ab, double ac, double bc) {
