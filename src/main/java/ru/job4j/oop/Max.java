92ff68b8cb98aae231e93ba98e4593493ca21cd1
vladimir <tintutin@list.ru>
2026-03-23 17:18:45 +0400

    Max

--- src/main/java/ru/job4j/oop/Max.java
+++ src/main/java/ru/job4j/oop/Max.java
@@ -6,10 +6,10 @@ public class Max {
     }
 
     public static int findMax(int a, int b, int c) {
-        return (a > b) ? (findMax(a, b)) : (findMax(b, c));
+        return findMax(a, findMax(b, c));
     }
 
     public static int findMax(int a, int b, int c, int d) {
-        return findMax(a, b) > findMax(c, d) ? findMax(a, b) : (findMax(c, d));
+        return findMax(a, findMax(b, findMax(c, d)));
     }
 }
