fb4fea8bb0b9a11ee1084453773cab5814a7fa41
vladimir <tintutin@list.ru>
2026-03-23 22:50:39 +0400

    Tracker

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,31 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = new Item[size];
+        int newSize = 0;
+        for (int index = 0; index < items.length; index++) {
+            if (items[index] != null && items[index].getName() == key) {
+                result[newSize] = items[index];
+                newSize++;
+            }
+        }
+        result = Arrays.copyOf(result, newSize);
+        return result;
+    }
+
+    public Item[] findAll() {
+        Item[] result = new Item[size];
+        int newSize = 0;
+        for (int index = 0; index < items.length; index++) {
+            if (items[index] != null) {
+                result[newSize] = items[index];
+                newSize++;
+            }
+        }
+        result = Arrays.copyOf(result, newSize);
+        return result;
     }
 }
