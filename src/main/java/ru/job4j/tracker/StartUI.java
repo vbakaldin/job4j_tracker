a66a8e8b962a0370f8e419d84f338f9daaa196c2
vladimir <tintutin@list.ru>
2026-03-26 12:53:03 +0400

    ValidateInput

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,10 +1,8 @@
 package ru.job4j.tracker;
 
 import ru.job4j.tracker.action.*;
-import ru.job4j.tracker.input.Input;
-import ru.job4j.tracker.input.ValidateInput;
-import ru.job4j.tracker.output.ConsoleOutput;
-import ru.job4j.tracker.output.Output;
+import ru.job4j.tracker.input.*;
+import ru.job4j.tracker.output.*;
 
 public class StartUI {
     private final Output output;
@@ -36,7 +34,7 @@ public class StartUI {
 
     public static void main(String[] args) {
         Output output = new ConsoleOutput();
-        Input input = new ValidateInput();
+        Input input = new ValidateInput(output, new ConsoleInput());
         Tracker tracker = new Tracker();
         UserAction[] actions = {
                 new CreateAction(output),
