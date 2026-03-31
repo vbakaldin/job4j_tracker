3894bc42aac870634cbe7444e64469b9ccb93b5d
vladimir <tintutin@list.ru>
2026-03-31 18:29:33 +0400

    fix

--- src/main/java/ru/job4j/collection/UsageHashSet.java
+++ src/main/java/ru/job4j/collection/UsageHashSet.java
@@ -9,7 +9,7 @@ public class UsageHashSet {
         autos.add("BMW");
         autos.add("Volvo");
         autos.add("Toyota");
-        for(String auto : autos) {
+        for (String auto : autos) {
             System.out.println(auto);
         }
     }
