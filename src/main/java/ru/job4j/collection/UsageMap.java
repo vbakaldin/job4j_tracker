6bccac6057db6980a3d500118fbc801ae15c5164
vladimir <tintutin@list.ru>
2026-04-06 19:28:19 +0400

    UsageMap

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,9 @@ public class UsageMap {
         HashMap<String, String> map = new HashMap<>();
         map.put("parsentev@yandex.ru", "Petr Arsentev");
         map.put("vldimir_bakalind@yandex.ru", "Vladimir Bakaldin");
+        map.put("vldimir_bakalind@yandex.ru", "Vladimir Ivanov");
+        map.put("vldimir_Sidorov@yandex.ru", "Vladimir Sidorov");
+
 
         for (String key : map.keySet()) {
             String value = map.get(key);
