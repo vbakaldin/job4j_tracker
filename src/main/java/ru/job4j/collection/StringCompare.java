d0fa03d2956cc849fc7f6b07b6f45d8f874079d8
vladimir <tintutin@list.ru>
2026-04-07 22:48:51 +0400

    StringCompare

---
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class StringCompare implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        int result = 0;
+        int size = Math.min(left.length(), right.length());
+        for (int i = 0; i < size; i++) {
+            result = Character.compare(left.charAt(i), right.charAt(i));
+            if (result != 0) {
+                break;
+            }
+        }
+        if (result == 0 && left.length() != right.length()) {
+            result = Integer.compare(left.length(), right.length());
+        }
+        return result;
+    }
+}
