c56e4594c43034fbbc9e264fc20d2f0deb46d92e
vladimir <tintutin@list.ru>
2026-04-10 14:10:26 +0400

    DepartmentsDescComparator

--- src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -7,18 +7,9 @@ public class DepartmentsDescComparator implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        int result = 0;
         String[] s1 = left.split("/");
         String[] s2 = right.split("/");
-
-        int size = Math.min(s1.length, s2.length);
-        for (int i = 0; i < size; i++) {
-            result = i == 0 ? s2[i].compareTo(s1[i]) : s1[i].compareTo(s2[i]);
-            if (result != 0) {
-                break;
-            }
-        }
-
+        int result = s2[0].compareTo(s1[0]);
         return result != 0 ? result : left.compareTo(right);
     }
 }
