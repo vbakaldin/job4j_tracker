db890f8e78e0589188cf67fe198a23744f97b1e8
vladimir <tintutin@list.ru>
2026-04-11 10:52:59 +0400

    FunctionCalculator

---
+++ src/main/java/ru/job4j/function/FunctionCalculator.java
@@ -0,0 +1,18 @@
+package ru.job4j.function;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class FunctionCalculator {
+    public List<Double> diapason(int start, int end, Function<Double, Double> function) {
+        List<Double> result = new ArrayList<>();
+        for (int i = start; i < end; i++) {
+            double temp = function.apply((double) i);
+            System.out.println(temp);
+            result.add(function.apply((double) i));
+
+        }
+        return result;
+    }
+}
---
+++ src/test/java/ru/job4j/function/FunctionCalculatorTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.function;
+
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FunctionCalculatorTest {
+    @Test
+    void whenLinearFunctionThenLinearResults() {
+        FunctionCalculator function = new FunctionCalculator();
+        List<Double> result = function.diapason(5, 8, x -> 2 * x + 1);
+        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        assertThat(result).containsAll(expected);
+    }
+
+    @Test
+    void whenLinearFunctionThenQuadraticResults() {
+        FunctionCalculator function = new FunctionCalculator();
+        List<Double> result = function.diapason(5, 8, x -> 2 * x * x + 3 * x + 1);
+        List<Double> expected = Arrays.asList(66D, 91D, 120D);
+        assertThat(result).containsAll(expected);
+    }
+
+    @Test
+    void whenLinearFunctionThenExponentialResults() {
+        FunctionCalculator function = new FunctionCalculator();
+        List<Double> result = function.diapason(5, 8, x -> Math.pow(3, x));
+        List<Double> expected = Arrays.asList(243D, 729D, 2187D);
+        assertThat(result).containsAll(expected);
+    }
+}
