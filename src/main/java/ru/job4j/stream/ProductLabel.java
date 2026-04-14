b5a01df9fba148742f8370102eb2f413516579f5
vladimir <tintutin@list.ru>
2026-04-14 22:17:46 +0400

    fix

--- src/main/java/ru/job4j/stream/ProductLabel.java
+++ src/main/java/ru/job4j/stream/ProductLabel.java
@@ -3,7 +3,6 @@ package ru.job4j.stream;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
 public class ProductLabel {
     public List<String> generateLabels(List<Product> products) {
         return products.stream().filter(product -> product.getStandard() - product.getActual() <= 3)
--- src/test/java/ru/job4j/stream/SchoolTest.java
+++ src/test/java/ru/job4j/stream/SchoolTest.java
@@ -19,7 +19,7 @@ public class SchoolTest {
                 new Student(90, "Surname9")
         );
         School school = new School();
-        Predicate<Student> predicate = student -> student.getScore() >= 70 ;
+        Predicate<Student> predicate = student -> student.getScore() >= 70;
         List<Student> result = school.collect(students, predicate);
         List<Student> expected = new ArrayList<>();
         expected.add(new Student(70, "Surname7"));
