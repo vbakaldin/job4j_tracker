7924b6be55d9439368efdec0be25bb89f9cf9c45
vladimir <tintutin@list.ru>
2026-04-15 20:07:25 +0400

    School

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 
 public class School {
     public List<Student> collect(List<Student> students, Predicate<Student> predicate) {
-        return students.stream().filter(predicate).collect(Collectors.toList());
+        return students.stream().filter(predicate)
+                .collect(Collectors.toList());
     }
 }
