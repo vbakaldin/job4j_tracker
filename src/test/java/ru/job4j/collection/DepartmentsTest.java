b6d395491339d8c068814ebb4d85cf5fcff22b17
vladimir <tintutin@list.ru>
2026-04-17 16:26:25 +0400

    DepartmentsTest

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -9,7 +9,7 @@ class DepartmentsTest {
 
     @Test
     void whenMissed() {
-        List<String> input = List.of("k1/sk1", "k2/sk1");
+        List<String> input = Arrays.asList("k1/sk1", "k2/sk1");
         List<String> expected = List.of("k1", "k1/sk1", "k2", "k2/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result).containsSequence(expected);
@@ -17,7 +17,7 @@ class DepartmentsTest {
 
     @Test
     void whenNonChange() {
-        List<String> input = List.of("k1", "k1/sk1");
+        List<String> input = Arrays.asList("k1", "k1/sk1");
         List<String> expected = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result).containsSequence(expected);
@@ -25,7 +25,7 @@ class DepartmentsTest {
 
     @Test
     void whenMissedSomeDepartmentsCode() {
-        List<String> input = List.of(
+        List<String> input = Arrays.asList(
                 "K1/SK1/SSK1",
                 "K1/SK1/SSK2",
                 "K2/SK1/SSK1"
