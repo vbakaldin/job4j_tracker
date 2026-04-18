8df00ebaee1d0ce63d39dbb38883a1e82cf71370
vladimir <tintutin@list.ru>
2026-04-18 13:55:29 +0400

    Analyze

---
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -0,0 +1,56 @@
+package ru.job4j.stream;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Analyze {
+    public static double averageScore(Stream<Pupil> stream) {
+        return stream.flatMap(pupil -> pupil.subjects().stream())
+                .mapToInt(Subject::score)
+                .average()
+                .orElse(0.0);
+    }
+
+    public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+        return stream.map(pupil -> new Tuple(pupil.name(), pupil.subjects()
+                    .stream().mapToInt(Subject::score)
+                    .average()
+                    .orElse(0.0)))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
+        return stream.flatMap(pupil -> pupil.subjects().stream())
+                .collect(Collectors.groupingBy(
+                        Subject::name,
+                        LinkedHashMap::new,
+                        Collectors.averagingDouble(Subject::score)))
+                .entrySet()
+                .stream()
+                .map(entry -> new Tuple(entry.getKey(), entry.getValue()))
+                .collect(Collectors.toList());
+    }
+
+    public static Tuple bestStudent(Stream<Pupil> stream) {
+        return stream.map(pupil -> new Tuple(pupil.name(), pupil.subjects()
+                        .stream().mapToInt(Subject::score)
+                        .sum()))
+                .max((left, right) -> Double.compare(left.score(), right.score()))
+                .orElse(null);
+    }
+
+    public static Tuple bestSubject(Stream<Pupil> stream) {
+        return stream.flatMap(pupil -> pupil.subjects().stream())
+                .collect(Collectors.groupingBy(
+                        Subject::name,
+                        LinkedHashMap::new,
+                        Collectors.summingDouble(Subject::score)))
+                .entrySet()
+                .stream()
+                .map(entry -> new Tuple(entry.getKey(), entry.getValue()))
+                .max((left, right) -> Double.compare(left.score(), right.score()))
+                .orElse(null);
+    }
+}
